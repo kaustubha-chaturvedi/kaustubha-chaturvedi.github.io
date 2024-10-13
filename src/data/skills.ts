e440dbcccd498cb8ce2c6d662db80a0c4a8b462e
Kaustubha Chaturvedi <78296069+kaustubha-chaturvedi@users.noreply.github.com>
2024-10-14 00:42:51 +0530

    Update skills.ts

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -4,7 +4,7 @@ export const positions: Position[] = [
     {
         name: "Software Engineer",
         skills: [
-            { category: "Programming Languages", items: ["Python", "C/C++", "JavaScript", "Golang"] },
+            { category: "Programming Languages", items: ["Python", "C/C++", "JavaScript", "Golang", "SQL"] },
             { category: "Frameworks & Libraries", items: ["Django", "FastAPI", "Express.js", "Flask", "NextJS", "TensorFlow", "Keras", "Pandas"] },
             { category: "Databases", items: ["MySQL", "PostgreSQL", "MongoDB", "DynamoDB", "Redis"] },
             { category: "DevOps & Tools", items: ["Git", "Docker", "AWS", "Azure", "Kubernetes"] }
@@ -14,7 +14,7 @@ export const positions: Position[] = [
     {
         name: "AI / ML Engineer",
         skills: [
-            { category: "Programming Languages", items: ["Python", "C/C++", "JavaScript", "Golang", "Java"] },
+            { category: "Programming Languages", items: ["Python", "C/C++", "JavaScript", "Golang", "SQL"] },
             { category: "Frameworks & Libraries", items: ["TensorFlow", "Keras", "PyTorch", "CrewAI", "Serper", "Scikit-learn", "FastAPI", "LangChain", "Groq", "Deepgram", "OpenCV"] },
             { category: "AI/ML", items: ["Data Preprocessing", "Feature Engineering", "Model Evaluation", "NLP", "Generative AI", "RAG"] },
             { category: "Databases", items: ["MySQL", "PostgreSQL", "MongoDB", "DynamoDB", "Redis", "ChromaDB"] },
