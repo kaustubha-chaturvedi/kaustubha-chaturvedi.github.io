09d1af73d6c43b01da980ee4507c644affe59b22
Kaustubha Chaturvedi <78296069+kaustubha-chaturvedi@users.noreply.github.com>
2025-09-14 23:22:58 +0530

    add: gtag for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { JetBrains_Mono } from 'next/font/google'
+import Head from "next/head";
+import Script from "next/script";
 import '@/styles/globals.css'
 
 export const metadata: Metadata = {
@@ -20,6 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+       <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XHBC2B7TVE"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XHBC2B7TVE');
+          `}
+        </Script>
+      </Head>
       <body className={jbm.className}>
         {children}
       </body>
